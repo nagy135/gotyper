876321f4637aa5a354ab4c3c02cfe4b7768b6839
Viktor Nagy <viktor.nagy@01people.com>
2022-04-14 14:22:36 +0200

    refactor(app): prettier

--- app/src/components/game.tsx
+++ app/src/components/game.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import { useNavigate, useParams } from "react-router-dom";
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { TGame } from "../types";
 import Api from "../api";
 
@@ -11,53 +20,56 @@ export default function Games() {
   let { gameId } = useParams();
 
   useEffect(() => {
-    if (gameId) Api.getGame(Number(gameId)).then(game => setGame(game));
+    if (gameId) Api.getGame(Number(gameId)).then((game) => setGame(game));
   }, [gameId]);
 
   const redirectToGames = () => {
     navigate(`/`);
-  }
+  };
 
   return (
     <Container>
-      <Button
-        color="error"
-        onClick={redirectToGames}
-        variant="contained">Back</Button>
-      {game ?
+      <Button color="error" onClick={redirectToGames} variant="contained">
+        Back
+      </Button>
+      {game ? (
         <>
           <h1>{game.Name}</h1>
           <h2>{game.Done ? "Ended" : "In progress"}</h2>
-          {game.Players && game.Players.length ? <>
-            <h2>Players</h2>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>#</TableCell>
-                    <TableCell align="right">Name</TableCell>
-                    <TableCell align="right">Progress</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {game.Players.map((player: any) => (
-                    <TableRow
-                      key={player.ID}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {player.ID}
-                      </TableCell>
-                      <TableCell align="right">{player.Name}</TableCell>
-                      <TableCell align="right">{player.Progress}</TableCell>
+          {game.Players && game.Players.length ? (
+            <>
+              <h2>Players ({game.Players.length})</h2>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>#</TableCell>
+                      <TableCell align="right">Name</TableCell>
+                      <TableCell align="right">Progress</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </> : null}
-        </> : null
-      }
+                  </TableHead>
+                  <TableBody>
+                    {game.Players.map((player: any) => (
+                      <TableRow
+                        key={player.ID}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {player.ID}
+                        </TableCell>
+                        <TableCell align="right">{player.Name}</TableCell>
+                        <TableCell align="right">{player.Progress}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </>
+          ) : null}
+        </>
+      ) : null}
     </Container>
   );
-};
+}
--- app/src/components/games.tsx
+++ app/src/components/games.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ListItem from "@mui/material/ListItem";
 import Box from "@mui/material/Box";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import Api from "../api";
 import { TGame } from "../types";
 
@@ -18,20 +18,22 @@ export default function Games() {
   };
 
   useEffect(() => {
-    Api.getGames().then(games => setGames(games));
+    Api.getGames().then((games) => setGames(games));
   }, []);
 
   return (
     <Container>
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
         <nav aria-label="main mailbox folders">
           <List>
             {games.map((game) => {
-              return <ListItem disablePadding key={game.ID}>
-                <ListItemButton onClick={() => redirectToGame(game.ID)}>
-                  <ListItemText primary={game.Name} />
-                </ListItemButton>
-              </ListItem>
+              return (
+                <ListItem disablePadding key={game.ID}>
+                  <ListItemButton onClick={() => redirectToGame(game.ID)}>
+                    <ListItemText primary={game.Name} />
+                  </ListItemButton>
+                </ListItem>
+              );
             })}
           </List>
         </nav>
